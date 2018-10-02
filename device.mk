5d7882431667f47446f776ace147c5562d251e1a
Ethan Chen <intervigil@gmail.com>
2018-10-02 13:51:03 +0200

    whyred: Use Soong namespace to avoid device build conflicts
    Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39

--- device.mk
+++ device.mk
@@ -34,6 +34,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += device/xiaomi/whyred
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
