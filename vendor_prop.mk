d9d7cb26c7c99933c58ee5e16d539a18e133c224
LuK1337 <priv.luk@gmail.com>
2019-02-02 19:34:23 +0100

    whyred: Add debug IMS props to workaround CarrierConfig restrictions
    We support IMS features on carriers not listed in the CarrierConfig,
    so let's use the props to enable it everywhere.
    
    Change-Id: Id3ad71d522a22048f7ad86d89695ad262361632d

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -141,3 +141,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_app_phase_offset_ns=1500000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000
+
+# VoLTE
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1
