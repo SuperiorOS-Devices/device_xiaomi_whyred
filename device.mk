af26aed8f257c1e5afb4b4f4b1e1dcd1433bd4bf
Rashed Sahaji <asifrashed2@gmail.com>
2019-08-09 14:46:53 +0530

    whyred: Build ThermalController
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- device.mk
+++ device.mk
@@ -407,6 +407,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service \
     thermal.sdm660
+ 
+ # ThermalController app
+PRODUCT_PACKAGES += \
+    ThermalController
 
 # Touchscreen
 PRODUCT_PACKAGES += \
