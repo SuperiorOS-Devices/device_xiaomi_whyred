f26a425635372e1fa790f31fd21edce6500fac69
srfarias <erikofarias@gmail.com>
2019-12-30 23:18:49 +0000

    whyred: build miui cam
    Change-Id: Ic2bc20c7bf6c35d321de912877d332897a01b9ab
    Signed-off-by: srfarias <erikofarias@gmail.com>

--- lineage_whyred.mk
+++ lineage_whyred.mk
@@ -25,6 +25,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Inherit from whyred device
 $(call inherit-product, device/xiaomi/whyred/device.mk)
 
+# Inherit from custom vendor
+$(call inherit-product, vendor/MiuiCamera/config.mk)
+
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
