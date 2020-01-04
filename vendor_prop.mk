c83cf7cf9f73101d389ed7dc02a37a2b77d822c4
srfarias <erikofarias@gmail.com>
2020-01-04 10:52:06 -0300

    whyred: reorganize radio props
    Signed-off-by: srfarias <erikofarias@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -3,13 +3,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.mcd_config_file=/system/etc/mcd_default.conf \
     drm.service.enabled=true \
     vendor.video.disable.ubwc=1 \
-    persist.radio.multisim.config=dsds \
     persist.vendor.qcomsysd.enabled=1 \
     ro.vendor.extension_library=libqti-perfd-client.so \
-    persist.vendor.radio.apm_sim_not_pwdn=1 \
-    persist.vendor.radio.sib16_support=1 \
-    persist.vendor.radio.custom_ecc=1 \
-    persist.vendor.radio.rat_on=combine \
     sys.vendor.shutdown.waittime=500 \
     ro.build.shutdown_timeout=0 \
     ro.frp.pst=/dev/block/bootdevice/by-name/frp \
@@ -158,12 +153,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_gl_backpressure=1
 
-# VoLTE/IMS
+# Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.ims_volte_enable=1 \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.radio.aosp_usr_pref_sel=true \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
+    persist.radio.multisim.config=dsds \
+    persist.vendor.radio.sib16_support=1 \
+    persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.data_ltd_sys_ind=1 \
     persist.radio.calls.on.ims=1 \
