b2abd70686a084e8b1f118fb6b7a57a2adff7eb1
dianlujitao <dianlujitao@lineageos.org>
2020-01-31 11:12:17 +0800

    whyred: Format update-sha1sums.py with yapf
    Change-Id: I396ad6d79ea6e47ba8b9a56da317123c7579aa67
    Signed-off-by: sreekfreak995 <sreekfreak995@gmail.com>

--- update-sha1sums.py
+++ update-sha1sums.py
@@ -23,24 +23,31 @@ from hashlib import sha1
 device='whyred'
 vendor='xiaomi'
 
-with open('proprietary-files.txt', 'r') as f:
-    lines = f.read().splitlines()
+lines = [line for line in open('proprietary-files.txt', 'r')]
 vendorPath = '../../../vendor/' + vendor + '/' + device + '/proprietary'
 needSHA1 = False
 
 
 def cleanup():
     for index, line in enumerate(lines):
+        # Remove '\n' character
+        line = line[:-1]
+
         # Skip empty or commented lines
-        if len(line) == 0 or line[0] == '#' or '|' not in line:
+        if len(line) == 0 or line[0] == '#':
             continue
 
         # Drop SHA1 hash, if existing
-        lines[index] = line.split('|')[0]
+        if '|' in line:
+            line = line.split('|')[0]
+            lines[index] = '%s\n' % (line)
 
 
 def update():
     for index, line in enumerate(lines):
+        # Remove '\n' character
+        line = line[:-1]
+
         # Skip empty lines
         if len(line) == 0:
             continue
@@ -53,15 +60,16 @@ def update():
         if needSHA1:
             # Remove existing SHA1 hash
             line = line.split('|')[0]
+            filePath = line.split(':')[1] if len(
+                line.split(':')) == 2 else line
 
-            filePath = line.split(';')[0].split(':')[-1]
             if filePath[0] == '-':
-                filePath = filePath[1:]
+                file = open('%s/%s' % (vendorPath, filePath[1:]), 'rb').read()
+            else:
+                file = open('%s/%s' % (vendorPath, filePath), 'rb').read()
 
-            with open(os.path.join(vendorPath, filePath), 'rb') as f:
-                hash = sha1(f.read()).hexdigest()
-
-            lines[index] = '%s|%s' % (line, hash)
+            hash = sha1(file).hexdigest()
+            lines[index] = '%s|%s\n' % (line, hash)
 
 
 if len(sys.argv) == 2 and sys.argv[1] == '-c':
@@ -70,4 +78,7 @@ else:
     update()
 
 with open('proprietary-files.txt', 'w') as file:
-    file.write('\n'.join(lines) + '\n')
+    for line in lines:
+        file.write(line)
+
+    file.close()
