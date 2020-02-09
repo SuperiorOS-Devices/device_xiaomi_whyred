205d24ee4582a0acae1401aa863f2e5c6ac1c37b
srfarias <erikofarias@gmail.com>
2020-02-09 15:05:53 +0000

    whyred: disable apex
    Change-Id: Icd50cc52a7c15d5566e51dc3670c9111333ba659
    Signed-off-by: srfarias <erikofarias@gmail.com>

--- lineage_whyred.mk
+++ lineage_whyred.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
 # Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Inherit from whyred device
 $(call inherit-product, device/xiaomi/whyred/device.mk)
