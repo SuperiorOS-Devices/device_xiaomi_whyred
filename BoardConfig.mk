e6e1b525059772c0baf2502a9977e3604b5ced72
RevanthTolety <crevanthtolety@gmail.com>
2020-03-08 01:18:03 +0530

    Whyred: Ignore Selinux Neverallows
    Signed-off-by: sreekfreak995 <sreekfreak995@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -202,6 +202,11 @@ BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
+
 # Treble
 BOARD_VNDK_RUNTIME_DISABLE := true
 BOARD_VNDK_VERSION := current
