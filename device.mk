807057c42710be37dcf3ed3d582d1cdd40ffa1d5
sreekfreak995 <sreekfreak995@gmail.com>
2020-05-29 13:52:18 +0530

    Inherit dirac blobs from vendor
    Signed-off-by: sreekfreak995 <sreekfreak995@gmail.com>

--- device.mk
+++ device.mk
@@ -26,6 +26,9 @@ $(call inherit-product, vendor/xiaomi/whyred/whyred-vendor.mk)
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
+# Dirac
+$(call inherit-product, vendor/xiaomi/whyred/dirac/dirac.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
