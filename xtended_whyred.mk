22545a78d6f3f02454af8a290a26c55f03b27d37
sreekfreak995 <sreekfreak995@gmail.com>
2020-06-26 21:33:31 +0530

    Address Memecam denial
    Also change path
    
    Signed-off-by: sreekfreak995 <sreekfreak995@gmail.com>

--- xtended_whyred.mk
+++ xtended_whyred.mk
@@ -29,7 +29,7 @@ $(call inherit-product, device/xiaomi/whyred/device.mk)
 $(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
 # Inherit from custom vendor
-$(call inherit-product, vendor/MiuiCamera/config.mk)
+$(call inherit-product, vendor/xiaomi/MiuiCamera/config.mk)
 
 PRODUCT_NAME := xtended_whyred
 PRODUCT_BRAND := Xiaomi
