e37a69fc18904f2cd9fe93e3d3fea6797586ac43
LuK1337 <priv.luk@gmail.com>
2020-09-18 13:12:09 +0200

    whyred: Drop android.hardware.thermal HAL
    android.hardware.thermal@1.0::IThermal/default is deprecated in compatibility matrix at FCM Version 4; it should not be served.
    
    Change-Id: Icc3c672a0bf6b9e4ee76ccf91e6cdca5fb778fde
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- device.mk
+++ device.mk
@@ -481,13 +481,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.1 \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sdm660
-
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
