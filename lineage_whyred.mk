d8cb0a0a9d73d13114b6356232ec5cdb9c38838c
SakilMondal <mondalsakil@gmail.com>
2021-10-10 23:00:17 +0530

    whyred: Update Build Fingerprint from Redfin SPB5.210812.002
    Change-Id: Iee30bad78f0b232c68e72b89c2dd7245e344e759

--- lineage_whyred.mk
+++ lineage_whyred.mk
@@ -26,6 +26,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := whyred
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.211001.001 7641976 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 12 SPB5.210812.002 7671067 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:12/SPB5.210812.002/7671067:user/release-keys
