f762a3e56586889273b0934ca196b11300e24639
Sonal Singh <sonal.singh.19993@gmail.com>
2023-10-12 18:30:32 +0200

    whyred: prebuilts: Add GoogleCameraGo
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- device.mk
+++ device.mk
@@ -221,6 +221,12 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+# Gcam
+PRODUCT_PACKAGES += \
+    GoogleCameraGo
+
+
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
