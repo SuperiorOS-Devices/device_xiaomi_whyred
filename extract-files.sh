18564e00553b780726d4946558427b7bdbd8bbfe
Anand Shekhawat <anandsingh215@yahoo.com>
2023-10-14 13:05:53 +0200

    whyred: patch libwvhidl.so to load libprotobuf-cpp-full-3.9.1.so

--- extract-files.sh
+++ extract-files.sh
@@ -74,6 +74,9 @@ function blob_fixup() {
         vendor/lib64/libgf_ca.so)
             sed -i 's|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g' "${2}"
             ;;
+        vendor/lib64/libwvhidl.so)
+            "${PATCHELF}" --replace-needed libprotobuf-cpp-lite-3.9.1.so libprotobuf-cpp-full-3.9.1.so "${2}"
+            ;;
     esac
 }
 
