8507681d6f0b8e4907dd2aed60ddb60f4afdfc50
Bruno Martins <bgcngm@gmail.com>
2024-02-08 17:05:32 +0000

    whyred: Use the common device compatibility matrix from QCOM
    Change-Id: I0c0f3ecddbe6d7b232cc0d13c78ca58094e4e720
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,7 +99,7 @@ LOC_HIDL_VERSION := 3.0
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/device_framework_compatibility_matrix.xml
 
 # Init
