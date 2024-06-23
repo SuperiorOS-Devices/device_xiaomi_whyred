78c3b3fe72d0d691f94a58a57b9fbf1e73f3c5bc
Arijit78 <sahaarijit2007@gmail.com>
2024-06-23 18:06:57 +0000

    whyred: Add device framework compatibility matrix
    - Needed to pass vintf check with target level 5
    - Generated with https://gist.github.com/SebaUbuntu/ec053a00a9988eaea091fb1cc1f19324
    
    Co-authored-by: SebaUbuntu <seba@sebaubuntu.dev>
    Signed-off-by: Arijit78 <sahaarijit2007@gmail.com>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,6 +100,7 @@ LOC_HIDL_VERSION := 3.0
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/device_framework_compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_sdm660
